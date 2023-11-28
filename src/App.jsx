054063db09cb6221f9adbd9d6f327337b966085c
joseph-kinyuru <josephkinyuru22@gmail.com>
2023-11-28 16:03:58 +0300

    Sitemap removed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom'
 import Index from './Pages/Index';
 import NotFound from './Pages/NotFound';
 import Resume from './Pages/Resume';
-import Sitemap from './Pages/Sitemap';
 
 export default function App() {
 
@@ -11,7 +10,6 @@ export default function App() {
     <Routes>
       <Route exact path="/" element={<Index />} />
       <Route exact path="/resume" element={<Resume />} />
-      <Route exact path="/sitemap" element={<Sitemap />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
 
