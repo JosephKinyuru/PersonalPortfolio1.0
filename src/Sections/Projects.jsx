d55ce785fbb0378e5c74ca8e1be77cee2db2a201
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-01-23 13:07:15 +0300

    Added threads to projects.

--- src/Sections/Projects.jsx
+++ src/Sections/Projects.jsx
@@ -6,6 +6,7 @@ import { Fade } from 'react-awesome-reveal';
 import AutoTrackImage from '/src/Assets/AutoTrack.png';
 import CookingConfessionsImage from '/src/Assets/CookingConfessions.png';
 import StoryCircleImage from '/src/Assets/StoryCircle.png';
+import ThreadsImage from '/src/Assets/Threads.png';
 
 
 export default function Projects() {
@@ -23,6 +24,15 @@ export default function Projects() {
       const sampleProjects = [
         {
             date: '1',
+            title: 'Threads',
+            cover: ThreadsImage, 
+            github: 'https://github.com/JosephKinyuru/threads2',
+            external: 'https://threads2-three.vercel.app/',
+            tech: ['Next.JS', 'MongoDB', 'Typescrit','Tailwindcss'],
+            html:'Threads is a duplicate of the app threads by meta. I developed the project in order to test and develop my skills with NextJS and cloud computing with MongoDB. Feel free to check it out its works really well for a social app.',
+        },
+        {
+            date: '2',
             title: 'Cooking confessions',
             cover: CookingConfessionsImage, 
             github: 'https://github.com/JosephKinyuru/CookingConfessions',
@@ -31,7 +41,7 @@ export default function Projects() {
             html:'Cooking confessions has a curated data set of different recipes which are catehorized into four categories. Users are able to view the available recipes after loading the site and can then click to view one full recipe in a different section and then add a comment to it. Users can also choose which category to view.',
         },
         {
-            date: '2',
+            date: '3',
             title: 'Story Circle',
             cover: StoryCircleImage, 
             github: 'https://github.com/JosephKinyuru/Story-Circle',
@@ -40,7 +50,7 @@ export default function Projects() {
             html:'Story Circle is a social platform connecting book enthusiasts, enabling them to create and join book clubs. Users can engage in discussions, explore a rich library of books, and share their insights through comments.',        
         },
         {
-            date: '3',
+            date: '4',
             title: 'Auto Track',
             cover: AutoTrackImage, 
             github: 'https://github.com/barondevke/AutoTrackFinal',
